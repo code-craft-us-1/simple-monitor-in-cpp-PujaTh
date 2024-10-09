80ce1489adf4c21a28cdf0fcfcfbebf1f73c351d
PujaTh <51761288+PujaTh@users.noreply.github.com>
2024-10-09 19:09:53 +0000

    try2

--- monitor.cpp
+++ monitor.cpp
@@ -4,14 +4,16 @@
 #include <chrono>
 #include <iostream>
 using std::cout, std::flush, std::this_thread::sleep_for, std::chrono::seconds;
-
+void pattern(std::string pat)
+{
+cout<<pat<<flush;
+sleep_for(seconds(1));
+}
 void displayMessage(std::string msg) {
     cout << msg;
     for (int i = 0; i < 6; i++) {
-        cout << "\r* " << flush;
-        sleep_for(seconds(1));
-        cout << "\r *" << flush;
-        sleep_for(seconds(1));
+        pattern("\r*");
+        pattern("\r *");
     }
 }
 
