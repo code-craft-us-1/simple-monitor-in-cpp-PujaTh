204663ea9e3ea2360ebea3258195a0a34e880d62
PujaTh <51761288+PujaTh@users.noreply.github.com>
2024-10-09 19:35:20 +0000

    ch

--- monitor.cpp
+++ monitor.cpp
@@ -9,21 +9,46 @@ cout << pat << flush;
 sleep_for(seconds(1));
 }
 void displayMessage(std::string msg) {
-    cout << msg;
+    report(msg);
     for (int i = 0; i < 6; i++) {
         pattern("\r*");
         pattern("\r *");
     }
 }
 
+void report(std::string entry)
+{
+  cout<<entry;
+}
+bool criticalTemp(float temperature)
+{
+  if(temperature > 102 || temperature < 95)
+    return true;
+  else
+    return false;
+}
+bool criticalPulseRate(float pulse)
+{
+ if (pulseRate < 60 || pulseRate > 100)
+  return true;
+else
+  return false;
+}
+bool ciriticalSpoc(float spoc2)
+{
+  if (spo2 < 90)
+    return true;
+  else
+   return false;
+}
 int vitalsOk(float temperature, float pulseRate, float spo2) {
-  if (temperature > 102 || temperature < 95) {
+  if (criticalTemp(temperature)) {
     displayMessage("Temperature is critical!\n");
     return 0;
-  } else if (pulseRate < 60 || pulseRate > 100) {
+  } else if (criticalPulseRate(pulseRate)) {
     displayMessage("Pulse Rate is out of range!\n");
     return 0;
-  } else if (spo2 < 90) {
+  } else if (ciriticalSpoc(spo2)) {
     displayMessage("Oxygen Saturation out of range!\n");
     return 0;
   }
